58ec28bb4630139bc33efd5a2dd53ba1534b2910
Tom Daniel <0xtomdaniel@gmail.com>
2025-03-04 15:14:53 +0800

    add ember sdk package to aave agent

--- examples/aave-agent/index.ts
+++ examples/aave-agent/index.ts
@@ -1,20 +1,11 @@
-import { Chain } from "../../src/adapters/providers/aave/chain";
-import { Agent } from "./agent";
+import { Agent } from "./agent.js";
 import { ethers } from "ethers";
 import * as dotenv from "dotenv";
-import { EmberGrpcClient } from "../../src/lib/client/ember-client.js";
+import { EmberGrpcClient } from "@emberai/sdk-typescript";
 
 dotenv.config();
 
-// sepolia testnet:
-
-// const rpc = 'https://eth-sepolia.blastapi.io/58417139-0bc7-49b3-9637-ae50b6ecd82b'
-// const chainId = 11155111;
-
-// arbitrum one:
-
 const rpc = process.env.AAVE_RPC_URL || "https://arbitrum.llamarpc.com";
-const chainId = parseInt(process.env.AAVE_CHAIN_ID || "42161");
 const endpoint = process.env.EMBER_ENDPOINT || "grpc.api.emberai.xyz:50051";
 
 const main = async () => {
@@ -26,9 +17,7 @@ const main = async () => {
   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
   console.log(`Using wallet ${wallet.address}`);
 
-  const chain = new Chain(chainId, rpc); // TODO: can't types from AAVE adapter
-
-  const provider = chain.getProvider();
+  const provider = new ethers.providers.JsonRpcProvider(rpc);
   const signer = wallet.connect(provider);
   const client = new EmberGrpcClient({ endpoint });
   const agent = new Agent(client, signer, wallet.address);
