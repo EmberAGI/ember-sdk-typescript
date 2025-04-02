0f4cef94b16c3f6dcc3a29ce02053e9e2675c2a7
Vladimir Kalnitsky <klntsky@gmail.com>
2025-04-03 01:21:01 +0400

    Add integration tests for dynamic AAVE API

---
+++ test/helpers/permutations.ts
@@ -0,0 +1,17 @@
+import { pipe } from 'fp-ts/function';
+import * as A from 'fp-ts/Array';
+
+export default function permutations<T>(as: T[]): T[][] {
+  if (as.length === 0) return [[]];
+
+  return pipe(
+    as,
+    A.chain((a, i) =>
+      pipe(
+        [...as.slice(0, i), ...as.slice(i + 1)],
+        permutations,
+        A.map(rest => [a, ...rest])
+      )
+    )
+  );
+}
